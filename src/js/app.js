91a7da13473ddfc2f6148c9173d022df371c1042
Mle4nbly <mle4nbly@gmail.com>
2024-12-19 12:07:34 +0700

    lint fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,29 @@
 import Field from "./field/field.js";
 
 export default class App {
+  static init() {
+    this.element = document.querySelector(".field");
+    this.field = new Field(this.element);
 
-    static init() {
-        this.element = document.querySelector('.field');
-        this.field = new Field(this.element);
+    this.field.generateField();
+    this.createInterval();
+  }
 
-        this.field.generateField();
-        this.createInterval();
+  static createInterval() {
+    let lastIndex;
+    let index;
+
+    const newHoleInterval = setInterval(() => {
+      do {
+        index = Math.floor(Math.random() * 16);
+      } while (index === lastIndex);
+
+      lastIndex = index;
+      this.field.activateHole(index);
+    }, 1000);
+
+    if (newHoleInterval) {
+      console.log("Интервал запущен");
     }
-
-    static createInterval() {
-        let lastIndex;
-        let index;
-
-        const newHoleInterval = setInterval(() => {
-            do {
-                index = Math.floor(Math.random() * 16);
-            } while (index === lastIndex);
-
-            lastIndex = index;
-            this.field.activateHole(index);
-        }, 1000);
-
-        if (newHoleInterval) {
-        console.log("Интервал запущен");
-        }
-    }
+  }
 }
--- src/js/field/field.js
+++ src/js/field/field.js
@@ -1,37 +1,37 @@
 export default class Field {
-    constructor(element) {
-        if (!(element instanceof HTMLElement)) {
-            throw new Error("Ожидался DOM элемент");
-        }
-        this.element = element;
-        this.activeHole;
-        this.holes;
+  constructor(element) {
+    if (!(element instanceof HTMLElement)) {
+      throw new Error("Ожидался DOM элемент");
+    }
+    this.element = element;
+    this.activeHole;
+    this.holes;
+  }
+
+  activateHole(index) {
+    if (!this.holes[index]) {
+      throw new Error("Такого элемента в массиве нет");
     }
 
-    activateHole(index) {
-        if (!this.holes[index]) {
-            throw new Error("Такого элемента в массиве нет");
-        }
+    this.deactivateHole(this.activeHole);
 
-        this.deactivateHole(this.activeHole);
+    this.holes[index].classList.add("hole_active");
+    this.activeHole = this.holes[index];
+  }
 
-        this.holes[index].classList.add("hole_active");
-        this.activeHole = this.holes[index];
+  deactivateHole(activeHole) {
+    if (activeHole) {
+      activeHole.classList.remove("hole_active");
+    }
+  }
+
+  generateField() {
+    for (let index = 0; index <= 15; index++) {
+      const hole = document.createElement("div");
+      hole.className = "hole";
+      this.element.appendChild(hole);
     }
 
-    deactivateHole(activeHole) {
-        if (activeHole) {
-            activeHole.classList.remove("hole_active");
-        }   
-    }
-
-    generateField() {
-        for (let index = 0; index <= 15; index++) {
-            const hole = document.createElement("div");
-            hole.className = "hole";
-            this.element.appendChild(hole);
-        }
-
-        this.holes = this.element.querySelectorAll('.hole');
-    }
+    this.holes = this.element.querySelectorAll(".hole");
+  }
 }
